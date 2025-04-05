d3ef8edde8f707d894b9da230c0a7f72a79aee60
Timo Eberl <timo@timoeberl.de>
2025-04-05 17:32:40 +0200

    screen resolution and scaling settings, adjust slices, first slice, add player to map, add player collisions

--- src/map.gd
+++ src/map.gd
@@ -1,5 +1,6 @@
 extends TileMapLayer
 
+@export var first_slice : PackedScene
 @export var slices : Array[PackedScene]
 @export var number_of_slices := 10
 
@@ -7,11 +8,15 @@ extends TileMapLayer
 var _height := 0
 
 func _ready() -> void:
+	var first_slice_instance : TileMapSlice = first_slice.instantiate()
+	add_slice_to_map(first_slice_instance)
+	first_slice_instance.free() # not sure if necessary
+	
 	for i in number_of_slices:
 		randomize() # Randomizes the seed (or the internal state) of the random number generator
-		var slice : TileMapSlice = slices[randi() % slices.size()].instantiate()
-		add_slice_to_map(slice)
-		slice.free() # not sure if necessary
+		var slice_instance : TileMapSlice = slices[randi() % slices.size()].instantiate()
+		add_slice_to_map(slice_instance)
+		slice_instance.free() # not sure if necessary
 
 func add_slice_to_map(slice : TileMapSlice):
 	var w := slice.get_width()
