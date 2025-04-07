73da46a9122eea4265d3c9f306b30431ef05a370
Timo Eberl <timo@timoeberl.de>
2025-04-08 01:13:37 +0200

    different unlock requirements for levels

--- src/upgrade_counter.gd
+++ src/upgrade_counter.gd
@@ -1,9 +1,12 @@
 class_name UpgradeCounter
 extends Node2D
 
-@export var points_for_upgrade_laser := 10
-@export var points_for_upgrade_rocket := 5
+#@export var points_for_upgrade_laser := 10
+#@export var points_for_upgrade_rocket := 5
 @onready var laser_parent : Node2D = $"../Submarine/LaserSpriteParent3"
+
+@export var points_for_upgrade_laser : Array[int]
+@export var points_for_upgrade_rocket : Array[int]
 
 
 var laser_level := 0
@@ -17,10 +20,6 @@ enum Weapon {
 }
 	
 func on_laser_levelup():
-	laser_level += 1
-	%UI.update_laser_level(laser_level)
-	laser_level_progress -= points_for_upgrade_laser
-	%UI.update_laser_level_progress(laser_level_progress, points_for_upgrade_laser)
 	var new_laser = laser_parent.duplicate()
 	var dir = 1
 	if laser_level % 2 == 0:
@@ -35,17 +34,21 @@ func on_laser_levelup():
 func progress(weapon : Weapon):
 	if weapon == Weapon.LASER:
 		laser_level_progress += 1
-		%UI.update_laser_level_progress(laser_level_progress, points_for_upgrade_laser)
-		if laser_level_progress >= points_for_upgrade_laser:
+		%UI.update_laser_level_progress(laser_level_progress, points_for_upgrade_laser[laser_level])
+		if laser_level_progress >= points_for_upgrade_laser[laser_level]:
+			laser_level_progress -= points_for_upgrade_laser[laser_level]
+			laser_level += 1
+			%UI.update_laser_level(laser_level)
+			%UI.update_laser_level_progress(laser_level_progress, points_for_upgrade_laser[laser_level])
 			on_laser_levelup()
 			
 			
 			
 	if weapon == Weapon.ROCKET:
 		rocket_level_progress += 1
-		%UI.update_rocket_level_progress(rocket_level_progress, points_for_upgrade_rocket)
-		if rocket_level_progress >= points_for_upgrade_rocket:
+		%UI.update_rocket_level_progress(rocket_level_progress, points_for_upgrade_rocket[rocket_level])
+		if rocket_level_progress >= points_for_upgrade_rocket[rocket_level]:
+			rocket_level_progress -= points_for_upgrade_rocket[rocket_level]
 			rocket_level += 1
 			%UI.update_rocket_level(rocket_level)
-			rocket_level_progress -= points_for_upgrade_rocket
-			%UI.update_rocket_level_progress(rocket_level_progress, points_for_upgrade_rocket)
+			%UI.update_rocket_level_progress(rocket_level_progress, points_for_upgrade_rocket[rocket_level])
